4d82e89dfd8e520247f903b76c17c9c783fda460
Shivakumara Bannimatti <shivaslb72@gmail.com>
2020-05-05 09:00:02 +0530

    Updates

---
+++ precache-manifest.e4aa38488fa339465b1fe2d4f5252e0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d23121c772d33c179ad3629db8d267",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "14b538b7eeb9a2085947",
+    "url": "/portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f8ce9629cfee5ee0b6d3",
+    "url": "/portfolio/static/css/main.eff86c0f.chunk.css"
+  },
+  {
+    "revision": "14b538b7eeb9a2085947",
+    "url": "/portfolio/static/js/2.b9b77c71.chunk.js"
+  },
+  {
+    "revision": "5657dca68abcbbcb8243ceba173d3c7e",
+    "url": "/portfolio/static/js/2.b9b77c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ce9629cfee5ee0b6d3",
+    "url": "/portfolio/static/js/main.3806a4b4.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
